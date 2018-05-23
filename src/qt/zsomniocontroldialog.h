ea84f14ca6b14fb0dea8257bcd6b52fa4b493cff
root <root@vultr.guest>
2018-05-23 15:39:11 +0000

    final1

--- src/qt/zsomniocontroldialog.h
+++ src/qt/zsomniocontroldialog.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef ZTRTTCONTROLDIALOG_H
-#define ZTRTTCONTROLDIALOG_H
+#ifndef ZSOMNIOCONTROLDIALOG_H
+#define ZSOMNIOCONTROLDIALOG_H
 
 #include <QDialog>
 #include <QTreeWidgetItem>
